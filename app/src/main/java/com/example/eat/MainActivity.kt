29d7173dc288fc2efdcffd58215a5afab50d9243
molnahr <m.bali8@yahoo.com>
2020-11-15 12:40:41 +0200

    First Commit!

---
+++ app/src/main/java/com/example/eat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
